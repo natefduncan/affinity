34e6bb4f459fb9e7127dcfdab1c7194ac506ea97
Nate Duncan <natefduncan@gmail.com>
2022-05-31 16:50:06 -0500

    Add: DropdownOption, Location, Value, Interaction models

--- affinity/core/models.py
+++ affinity/core/models.py
@@ -1,5 +1,6 @@
 import datetime as dt
-from affinity.common.constants import ListType, ActionType
+from typing import List, Dict, Optional
+from affinity.common.constants import ListType, ActionType, PersonType
 
 #  https://api-docs.affinity.co/#lists
 class List: 
@@ -22,6 +23,13 @@ class ListEntry:
         self.created_at = created_at
 
 #  https://api-docs.affinity.co/#fields
+class DropdownOption:
+    def __init__(self, id: int, text: str, rank: int, color: int):
+        self.id = id
+        self.text = text
+        self.rank = rank
+        self.color = color
+
 class Field:
     def __init__(self, id: int, name: str, list_id: int, allows_multiple: bool, dropdown_options: List[DropdownOption], value_type: int):
         self.id = id
@@ -32,6 +40,23 @@ class Field:
         self.value_type = value_type
 
 #  https://api-docs.affinity.co/#the-field-value-resource
+class Location:
+    def __init__(self, street_address: str, city: str, state: str, country: str):
+        self.street_address = street_address
+        self.city = city
+        self.state = state
+        self.country = country
+
+class Value:
+    def __init__(self, dropdown: Optional[str], number: Optional[int], person: Optional[int], organization: Optional[int], location: Optional[Location], date: Optional[dt.datetime], text: Optional[str]):
+        self.dropdown = dropdown
+        self.number = number
+        self.person = person
+        self.organization = organization
+        self.location = location
+        self.date = date
+        self.text = text
+
 class FieldValue:
     def __init__(self, id: int, field_id: int, entity_id: int, list_entry_id: int, value: Value):
         self.id = id
@@ -49,3 +74,24 @@ class FieldValueChange:
         self.list_entry_id = list_entry_id
         self.action_type = action_type
         self.value = value
+
+#  https://api-docs.affinity.co/#persons
+class Interaction:
+    def __init__(self, date: dt.datetime, person_ids: List[int]):
+        self.date = date
+        self.person_ids = person_ids
+
+class Person:
+    def __init__(self, id: int, type: PersonType, first_name: str, last_name: str, emails: List[str], primary_email: str, organization_ids: List[int], opportunity_ids: List[int], list_entries: List[ListEntry], interaction_dates: Dict[str, dt.datetime], interactions: Dict[str, Interaction]): 
+        self.id = id
+        self.type = type
+        self.first_name = first_name
+        self.last_name = last_name
+        self.emails = emails
+        self.primary_email = primary_email
+        self.organization_ids = organization_ids
+        self.opportunity_ids = opportunity_ids
+        self.list_entries = list_entries
+        self.interaction_dates = interaction_dates
+        self.interactions = interactions
+
