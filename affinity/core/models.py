8cc1a582da72ef655a0d000ff18e224ec4d4fbab
Nate Duncan <natefduncan@gmail.com>
2022-05-31 16:21:40 -0500

    Add: ListEntry model

--- affinity/core/models.py
+++ affinity/core/models.py
@@ -1,6 +1,7 @@
+import datetime as dt
 from affinity.common.constants import ListType
 
-# https://api-docs.affinity.co/#lists
+#  https://api-docs.affinity.co/#lists
 class List: 
     def __init__(self, id: int, type: ListType, name: str, public: bool, owner_id: int, list_size: int):
         self.id = id
@@ -10,4 +11,12 @@ class List:
         self.owner_id = owner_id
         self.list_size = list_size
 
-
+#  https://api-docs.affinity.co/#list-entries
+class ListEntry:
+    def __init__(self, id: int, list_id: int, creator_id: int, entity_id: int, entity: Entity, created_at: dt.datetime):
+        self.id = id
+        self.list_id = list_id
+        self.creator_id = creator_id
+        self.entity_id = entity_id
+        self.entity = entity
+        self.created_at = created_at
