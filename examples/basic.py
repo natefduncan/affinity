4738a58b58cbe23c9ebb5761dffd754657327153
natefduncan <natefduncan@gmail.com>
2022-05-31 21:40:11 -0500

    Add: Basic example

---
+++ examples/basic.py
@@ -0,0 +1,8 @@
+import os
+from affinity import Client
+
+AFFINITY_TOKEN = os.getenv("AFFINITY_TOKEN", "") 
+
+if __name__=="__main__":
+    client = Client(AFFINITY_TOKEN)
+    print(client.lists.list().json())
