229db427a5433f9eebc5fb4ef21041b384ef02ec
natefduncan <natefduncan@gmail.com>
2022-06-11 07:36:07 -0500

    Chore: Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(
-        name='affinity',
-        version='1.0',
+        name='affinity-crm',
+        version='v1.0.0',
         description='Affinity CRM',
         author='Nathan Duncan',
-        author_email='',
-        packages=['affinity'],  # would be the same as name
-        install_requires=['requests==2.27.1', "dataclasses_json==0.5.7"], #external packages acting as dependencies
+        author_email='natefduncan@gmail.com',
+        url="https://github.com/natefduncan/affinity/archive/refs/tags/v1.0.0.tar.gz", 
+        packages=['affinity'],
+        install_requires=['requests==2.27.1', "dataclasses_json==0.5.7"],
         )
 
