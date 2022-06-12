45a9d045840d973ade2b2b13de0ec9e3e2561dd8
natefduncan <natefduncan@gmail.com>
2022-06-12 17:08:11 -0500

    Chore: Update minor version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
         name='affinity-crm',
-        version='v1.0.0',
+        version='v1.0.1',
         description='Affinity CRM',
         author='Nathan Duncan',
         author_email='natefduncan@gmail.com',
-        url="https://github.com/natefduncan/affinity/archive/refs/tags/v1.0.0.tar.gz", 
+        url="https://github.com/natefduncan/affinity/archive/refs/tags/v1.0.1.tar.gz", 
         packages=['affinity'],
         install_requires=['requests==2.27.1', "dataclasses_json==0.5.7"],
         )
