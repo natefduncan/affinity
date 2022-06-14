bd3e821e4b63ad92ca37d0ecd5eaaa398cca5ffc
Nate Duncan <natefduncan@gmail.com>
2022-06-14 10:58:39 -0500

    Refactor: Utilize fixtures

--- tests/test_field_value_changes.py
+++ tests/test_field_value_changes.py
@@ -1,9 +1,7 @@
 from affinity.core import models
 
-def test_field_value_changes_list(client):
-    many = client.lists().list()
-    test_list = [l for l in many if "test" in l.name.lower()][0]
-    fields = client.fields().list(list_id=test_list.id)
+def test_field_value_changes_list(client, opportunity_list):
+    fields = client.fields().list(list_id=opportunity_list.id)
     status_field = [i for i in fields if i.name == "Status"][0]
     field_value_changes = client.field_value_changes().list(field_id=status_field.id)
     assert isinstance(field_value_changes[0], models.FieldValueChange)
--- tests/test_field_values.py
+++ tests/test_field_values.py
@@ -1,10 +1,7 @@
-from affinity.common.constants import ListType
 from affinity.core import models
 
-def test_field_values_list(client):
-    many = client.lists().list()
-    test_list = [l for l in many if "test" in l.name.lower() and l.type == ListType.organization][0]
-    list_entry = client.list_entries(list_id=test_list.id).list()[0]
+def test_field_values_list(client, organization_list):
+    list_entry = client.list_entries(list_id=organization_list.id).list()[0]
     field_values = client.field_values().list(organization_id=list_entry.entity_id)
     assert isinstance(field_values[0], models.FieldValue)
 
