0eb6763ccab150714ccbf3b7219f7fae6c77277e
Nate Duncan <natefduncan@gmail.com>
2022-06-14 10:40:50 -0500

    Bug: Test list out of index

--- tests/test_field_value_changes.py
+++ tests/test_field_value_changes.py
@@ -7,7 +7,7 @@ AFFINITY_TOKEN = os.getenv("AFFINITY_TOKEN")
 
 def test_field_value_changes_list(client):
     many = client.lists().list()
-    test_list = [l for l in many if l.name.lower() == "test 2"][0]
+    test_list = [l for l in many if "test" in l.name.lower()][0]
     fields = client.fields().list(list_id=test_list.id)
     status_field = [i for i in fields if i.name == "Status"][0]
     field_value_changes = client.field_value_changes().list(field_id=status_field.id)
--- tests/test_field_values.py
+++ tests/test_field_values.py
@@ -1,13 +1,14 @@
 import os
 from affinity import Client
 from typing import List
+from affinity.common.constants import ListType
 from affinity.core import models
 
 AFFINITY_TOKEN = os.getenv("AFFINITY_TOKEN")
 
 def test_field_values_list(client):
     many = client.lists().list()
-    test_list = [l for l in many if l.name.lower() == "test"][0]
+    test_list = [l for l in many if "test" in l.name.lower() and l.type == ListType.organization][0]
     list_entry = client.list_entries(list_id=test_list.id).list()[0]
     field_values = client.field_values().list(organization_id=list_entry.entity_id)
     assert isinstance(field_values[0], models.FieldValue)
