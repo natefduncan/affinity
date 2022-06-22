370754b500e048caf22daaf3edc525de721dc570
Nate Duncan <natefduncan@gmail.com>
2022-06-22 16:21:56 -0500

    Refactor: Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
         name='affinity-crm',
-        version='v1.0.1',
+        version='v1.0.2',
         description='Affinity CRM',
         author='Nathan Duncan',
         author_email='natefduncan@gmail.com',
-        url="https://github.com/natefduncan/affinity/archive/refs/tags/v1.0.1.tar.gz", 
-        packages=['affinity'],
+        url="https://github.com/natefduncan/affinity", 
+        packages=['affinity', 'affinity.client', 'affinity.common', 'affinity.core'],
         install_requires=['requests==2.27.1', "dataclasses_json==0.5.7"],
         )
 
