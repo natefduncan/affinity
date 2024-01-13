727bca2b79b897a6ee96bbe75425899499f9b137
Mehmet Öner Yalçın <oneryalcin@gmail.com>
2024-01-13 15:08:18 +0000

    Updated package versions and bumped application version
    Updated versions for 'requests', 'dataclasses_json', and 'pytest' in requirements.txt. Additionally, increased the application version in setup.py to v1.1.0. These updates include important bug fixes, performance enhancements

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
         name='affinity-crm',
-        version='v1.0.2',
+        version='v1.1.0',
         description='Affinity CRM',
         author='Nathan Duncan',
         author_email='natefduncan@gmail.com',
         url="https://github.com/natefduncan/affinity", 
         packages=['affinity', 'affinity.client', 'affinity.common', 'affinity.core'],
-        install_requires=['requests==2.27.1', "dataclasses_json==0.5.7"],
+        install_requires=['requests==2.31.0', "dataclasses_json==0.6.3"],
         )
 
