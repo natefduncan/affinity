4a82336ad4a33504ead415577b2cb917132e5b03
Mehmet Öner Yalçın <oneryalcin@gmail.com>
2024-01-23 08:14:35 +0000

    Update package version in setup.py
    Updated the version number of the 'affinity-crm' package from 'v1.1.1' to 'v1.1.2' in the setup.py file. The version update reflects the addition of new features or bug fixes made to the software package.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='affinity-crm',
-        version='v1.1.1',
+        version='v1.1.2',
         description='Affinity CRM',
         author='Nathan Duncan',
         author_email='natefduncan@gmail.com',
